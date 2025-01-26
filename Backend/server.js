e2d393274965b8f4f5e703dfbfc6a3a9a804f86a
Roger <you@example.com23pwbcs0997@uetpeshawar.edu.pk>
2025-01-26 16:51:27 +0500

    Me

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
-            console.log("MongoDB connected successfully");
+            console.log("MongoDB connecd successfully");
         });
     })
     .catch(err => console.error(err));
